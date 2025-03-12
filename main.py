95cb38ae33a6a73ab6b9fba5fb7b7745c27110ee
Caro <carolina.cogo16@gmail.com>
2025-03-12 16:56:46 -0300

    Agrego main y moduloA

---
+++ main.py
@@ -0,0 +1,5 @@
+import moduloA
+print("Hola Mundo")
+print(f"__name__: {__name__}")
+a=moduloA.A()
+print(a.saludo())
---
+++ moduloA.py
@@ -0,0 +1,4 @@
+print(f"Hola soy el moduloA, mi __name__ es {__name__}")
+class A:
+    def saludo(self):
+        return "Hola desde la Clase A"
